434767c78d2ed2b829f4c1bbc05ce786b83d5948
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-12-16 05:33:46 +0200

    Enhance visualization and window management in threaded solver

--- src/main/java/com/example/nqueenssolver/solver/NQueensThread.java
+++ src/main/java/com/example/nqueenssolver/solver/NQueensThread.java
@@ -2,69 +2,127 @@ package com.example.nqueenssolver.solver;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
-public class NQueensThread extends Thread {
+public class NQueensThread extends SwingWorker<Void, int[]> {
     private final int threadNumber;
     private final int n;
+    private final Random random;
+    private final ChessboardPanel chessboardPanel; // Added ChessboardPanel instance
+    private final NQueensSolver solver;
 
-    public NQueensThread(int threadNumber, int n) {
+    public NQueensThread(int threadNumber, int n, Random random, ChessboardPanel chessboardPanel) {
         this.threadNumber = threadNumber;
         this.n = n;
+        this.random = random;
+        this.chessboardPanel = chessboardPanel;
+        this.solver = new NQueensSolver(n, random);
     }
-
     @Override
-    public void run() {
+    protected Void doInBackground() {
         System.out.println("Thread " + threadNumber + " started.");
 
         // Solve N-Queens problem for this thread
-        NQueensSolver solver = new NQueensSolver(n);
-        boolean solved = solver.solve();
-
-        if (solved) {
-            // Display the solution in a chessboard window
-            displayChessboard(solver.getQueens());
-        } else {
-            System.out.println("Thread " + threadNumber + " couldn't find a solution for N-Queens.");
-        }
+        solveWithVisualization();
 
         System.out.println("Thread " + threadNumber + " finished.");
-    }
 
-    private void displayChessboard(int[] queens) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Thread " + threadNumber + " Solution");
-            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            frame.setSize(400, 400);
+        // Display a message indicating the thread has finished
+        showMessage("Thread " + threadNumber + " has finished its task.");
 
-            // Set location relative to the previous frame with increased spacing
-            JFrame previousFrame = getPreviousFrame();
-            if (previousFrame != null) {
-                int offsetX = previousFrame.getX() + 40;  // Increase the horizontal spacing
-                int offsetY = previousFrame.getY() + 40;  // Increase the vertical spacing
-                frame.setLocation(offsetX, offsetY);
-            } else {
-                frame.setLocationRelativeTo(null); // Center the first frame on the screen
-            }
-
-            ChessboardPanel chessboardPanel = new ChessboardPanel(queens);
-            frame.add(chessboardPanel);
-
-            frame.setVisible(true);
-        });
-    }
-
-    private JFrame getPreviousFrame() {
-        Frame[] frames = JFrame.getFrames();
-        if (frames != null && threadNumber > 0 && frames.length > threadNumber - 1) {
-            return (JFrame) frames[threadNumber - 1];
-        }
         return null;
     }
 
-    private static class ChessboardPanel extends JPanel {
-        private final int[] queens;
+    private void showMessage(String message) {
+        SwingUtilities.invokeLater(() -> {
+            JOptionPane.showMessageDialog(null, message, "Thread Finished", JOptionPane.INFORMATION_MESSAGE);
+        });
+    }
+
+    private void solveWithVisualization() {
+        try {
+            solveNQueensWithVisualization(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void solveNQueensWithVisualization(int row) throws InterruptedException {
+        if (row == n) {
+            // All queens are placed successfully
+            return;
+        }
+
+        int[] shuffledColumns = getShuffledColumns();
+
+        for (int i = 0; i < n; i++) {
+            int col = shuffledColumns[i];
+            if (isSafe(row, col)) {
+                // Place the queen in this cell
+                solver.placeQueen(row, col);
+
+                // Publish intermediate state to update UI
+                publish(solver.getQueens().clone());
+
+                // Recur to place queens in the remaining rows
+                solveNQueensWithVisualization(row + 1);
+
+                // If placing queen in the current cell doesn't lead to a solution, backtrack
+                solver.removeQueen(row);
+                Thread.sleep(500);  // Adjust sleep duration for visualization speed
+            }
+        }
+    }
+
+    @Override
+    protected void process(java.util.List<int[]> chunks) {
+        int[] latestQueens = chunks.get(chunks.size() - 1);
+        updateChessboardDisplay(latestQueens);
+    }
+
+    private void updateChessboardDisplay(int[] queens) {
+        SwingUtilities.invokeLater(() -> {
+            chessboardPanel.updateQueens(queens);
+            chessboardPanel.repaint();
+        });
+    }
+
+    private int[] getShuffledColumns() {
+        int[] columns = new int[n];
+        for (int i = 0; i < n; i++) {
+            columns[i] = i;
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+
+            // Swap columns[i] and columns[j]
+            int temp = columns[i];
+            columns[i] = columns[j];
+            columns[j] = temp;
+        }
+
+        return columns;
+    }
+
+    private boolean isSafe(int row, int col) {
+        // Check if no queens are in the same column or diagonals
+        for (int i = 0; i < row; i++) {
+            if (solver.getQueens()[i] == col || Math.abs(solver.getQueens()[i] - col) == Math.abs(i - row)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static class ChessboardPanel extends JPanel {
+        private int[] queens;
 
         public ChessboardPanel(int[] queens) {
+            this.queens = queens;
+        }
+
+        public void updateQueens(int[] queens) {
             this.queens = queens;
         }
 
