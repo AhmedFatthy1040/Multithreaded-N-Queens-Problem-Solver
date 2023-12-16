163e21166b8a071d616e29d93c28c1406d2b242c
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-12-16 03:11:22 +0200

    Refactor NQueensSolverGUI for better window display and spacing.

--- src/main/java/com/example/nqueenssolver/NQueensSolverGUI.java
+++ src/main/java/com/example/nqueenssolver/NQueensSolverGUI.java
@@ -6,42 +6,57 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class NQueensSolverGUI extends JFrame {
-    private JTextField numberOfQueensField;
-    private JButton solveButton;
 
     public NQueensSolverGUI() {
-        setTitle("N-Queens Solver");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 150);
+        initComponents();
+    }
 
-        numberOfQueensField = new JTextField(10);
-        solveButton = new JButton("Solve");
+    private void initComponents() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("N-Queens Solver");
+
+        JLabel label = new JLabel("Enter the number of queens (n):");
+        JTextField textField = new JTextField(10);
+        JButton solveButton = new JButton("Solve");
 
         solveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                startSolving();
+                try {
+                    int n = Integer.parseInt(textField.getText());
+
+                    // Ensure n is a valid value
+                    if (n > 0) {
+                        solveNQueens(n);
+                    } else {
+                        JOptionPane.showMessageDialog(NQueensSolverGUI.this,
+                                "Please enter a positive integer for the number of queens.",
+                                "Invalid Input", JOptionPane.ERROR_MESSAGE);
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(NQueensSolverGUI.this,
+                            "Please enter a valid integer for the number of queens.",
+                            "Invalid Input", JOptionPane.ERROR_MESSAGE);
+                }
             }
         });
 
-        setLayout(new FlowLayout());
-        add(new JLabel("Enter the number of queens:"));
-        add(numberOfQueensField);
-        add(solveButton);
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.add(label);
+        panel.add(textField);
+        panel.add(solveButton);
 
-        setLocationRelativeTo(null);
-        setVisible(true);
+        getContentPane().add(panel);
+        pack();
+        setLocationRelativeTo(null); // Center the frame on the screen
     }
 
-    private void startSolving() {
-        int numberOfQueens = Integer.parseInt(numberOfQueensField.getText());
-
-        for (int queenNumber = 1; queenNumber <= numberOfQueens; queenNumber++) {
-            solveNQueensForThread(queenNumber, numberOfQueens);
+    private void solveNQueens(int n) {
+        for (int i = 0; i < n; i++) {
+            // Create a new thread for each solver
+            NQueensThread thread = new NQueensThread(i, n);
+            thread.start();
         }
-    }
-
-    private void solveNQueensForThread(int threadNumber, int numberOfQueens) {
-        new NQueensThread(threadNumber, numberOfQueens).start();
     }
 }
