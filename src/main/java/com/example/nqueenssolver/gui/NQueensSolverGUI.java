96b206ade35f999cc5e3c5dabff9a44290368570
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-12-16 05:34:20 +0200

    Refactor GUI layout and window positioning for threaded solutions

--- src/main/java/com/example/nqueenssolver/gui/NQueensSolverGUI.java
+++ src/main/java/com/example/nqueenssolver/gui/NQueensSolverGUI.java
@@ -1,14 +1,15 @@
 package com.example.nqueenssolver.gui;
 
-import javax.swing.*;
-
 import com.example.nqueenssolver.solver.NQueensThread;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class NQueensSolverGUI extends JFrame {
+    private static int offsetX = 0;  // Static variable to track horizontal offset
 
     public NQueensSolverGUI() {
         initComponents();
@@ -56,10 +57,41 @@ public class NQueensSolverGUI extends JFrame {
     }
 
     private void solveNQueens(int n) {
+        Random random = new Random();  // Create a Random instance
+
         for (int i = 0; i < n; i++) {
-            // Create a new thread for each solver
-            NQueensThread thread = new NQueensThread(i, n);
-            thread.start();
+            // Create a new thread for each solver with the Random instance and ChessboardPanel instance
+            NQueensThread.ChessboardPanel chessboardPanel = new NQueensThread.ChessboardPanel(new int[n]);
+            NQueensThread thread = new NQueensThread(i, n, random, chessboardPanel);
+            thread.execute(); // Use execute() instead of start() for SwingWorker
+
+            // Add the ChessboardPanel to the GUI with horizontal offset and thread number in the title
+            addChessboardPanelToGUI(chessboardPanel, i);
         }
+    }
+
+    private void addChessboardPanelToGUI(NQueensThread.ChessboardPanel chessboardPanel, int threadNumber) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Thread " + threadNumber + " Solution");
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame.setSize(400, 400);
+
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            int screenWidth = ge.getDefaultScreenDevice().getDisplayMode().getWidth();
+
+            // Set location relative to the previous frame with increased spacing
+            if (offsetX + 420 > screenWidth) {
+                // Start a new row
+                offsetX = 20;  // Reset horizontal offset
+            }
+
+            int offsetY = (offsetX == 20) ? 0 : 420;  // Determine vertical offset based on a new row or not
+            frame.setLocation(offsetX, offsetY);
+            offsetX += 420;  // Increase the horizontal spacing
+
+            frame.add(chessboardPanel);
+
+            frame.setVisible(true);
+        });
     }
 }
