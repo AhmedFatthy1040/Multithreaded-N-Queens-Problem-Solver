cdfc7671fb2d014c5fd7abe09abbae71abc41c29
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-12-16 03:32:36 +0200

    Updating Main class based on the project structure changes

--- src/main/java/com/example/nqueenssolver/Main.java
+++ src/main/java/com/example/nqueenssolver/Main.java
@@ -2,6 +2,8 @@ package com.example.nqueenssolver;
 
 import javax.swing.SwingUtilities;
 
+import com.example.nqueenssolver.gui.NQueensSolverGUI;
+
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
