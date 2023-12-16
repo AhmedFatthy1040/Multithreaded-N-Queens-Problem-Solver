dd8cd9b4943b59ea07f73b4c2939ea0d94149c44
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-12-16 02:19:32 +0200

    Create Main class for launching the N-Queens Solver application

--- src/main/java/com/example/nqueenssolver/Main.java
+++ src/main/java/com/example/nqueenssolver/Main.java
@@ -1,2 +1,14 @@
+package com.example.nqueenssolver;
 public class Main {
+
+    public static void main(String[] args) {
+        // Use SwingUtilities.invokeLater to ensure GUI is created on the event dispatch thread
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                // Create an instance of NQueensSolverGUI
+                NQueensSolverGUI gui = new NQueensSolverGUI();
+                gui.setVisible(true);
+            }
+        });
+    }
 }
